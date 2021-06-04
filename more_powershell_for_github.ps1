72ddb0ab4a932cb8d4adda7bd90e280db24e753f
Nico Hillé <nico.hille@student.ap.be>
2021-06-04 17:13:23 +0200

    This is a script with a function that gives back the authentication for the hashtable of a user. I made PSCredential and passed this on to a function. The function gave me an authentication as a return statement

---
+++ more_powershell_for_github.ps1
@@ -0,0 +1,27 @@
+﻿function Get-AuthHeader
+{
+
+param($C)
+
+$auth=[System.Convert]::ToBase64String([char[]]$C.GetNetworkCredential().Password)
+
+$headers = @{Authorization="Basic $auth"}
+
+return $headers
+
+}
+
+$user=Read-Host Geef uw username 
+
+Read-Host -AsSecureString -Prompt ’token’ | ConvertFrom-SecureString |
+Tee-Object .\secret.txt |
+ConvertTo-SecureString |
+Set-Variable ss_token
+$C = New-Object pscredential $user, $ss_token
+
+Set-GitHubAuthentication -SessionOnly `
+-Credential $C
+
+$authtable=Get-AuthHeader $C 
+
+$hashtable= Invoke-RestMethod -Headers $authtable https://api.github.com/user
