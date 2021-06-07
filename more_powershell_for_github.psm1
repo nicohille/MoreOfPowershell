d82102027c31098cd3e443cb3a60bbbc0ed4dbbc
Nico Hillé <nico.hille@student.ap.be>
2021-06-07 22:44:34 +0200

    I added a function get-gitcollaborator

--- more_powershell_for_github.psm1
+++ more_powershell_for_github.psm1
@@ -11,15 +11,34 @@ return $headers
 
 }
 
+function Add-GitHubCollaborator
+{
+$repo=read-host Aan welke repository wil je de collaborator aan toevoegen
+$collaborator=read-host Geef de user die je wil toevoegen
 
-$user = Read-Host "Geef uw username" 
+$C = New-Object System.Management.Automation.PSCredential($collaborator , $ss_token)
+
+Set-GitHubAuthentication -SessionOnly `
+-Credential $C
+
+$authcollab=Get-AuthHeader -Credential $C
+$permission=@{permission = "pull"} | ConvertTo-Json;
+
+$api= "https://api.github.com/repos/$repo/collaborators/$collaborator";
+
+Invoke-RestMethod -Uri $api -Headers $authcollab -ContentType "application/json" -Method Put -Body $permission
+
+}
+
+
+
+$user = "nico.hille@student.ap.be" 
 
 Read-Host -AsSecureString -Prompt ’token’ | ConvertFrom-SecureString |
 Tee-Object .\secret.txt |
 ConvertTo-SecureString |
 Set-Variable ss_token
-$C = New-Object pscredential $user, $ss_token
-Set-MoreGitHubAuthentication -Credential $C
+$C = New-Object System.Management.Automation.PSCredential('user' , $ss_token)
 
 Set-GitHubAuthentication -SessionOnly `
 -Credential $C
@@ -27,6 +46,12 @@ Set-GitHubAuthentication -SessionOnly `
 $authtable=Get-AuthHeader $C 
 
 $hashtable= Invoke-RestMethod -Headers $authtable https://api.github.com/user
-$hashtable
+
+$addcoll= Read-Host Wil je een collaborator toevoegen
+
+if($addcoll -eq "Ja")
+    {
+        Add-GitHubCollaborator
+    }
 
 
